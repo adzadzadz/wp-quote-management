38a1bf4b5fab1026fd460328f9f7765de48c7b12
Adrian T. Saycon <adzbite@gmail.com>
2025-06-19 15:40:17 +0800

    Added hooks to loader

--- includes/class-wqm-loader.php
+++ includes/class-wqm-loader.php
@@ -15,8 +15,10 @@ class WQM_Loader {
     public function run() {
         require_once __DIR__ . '/class-wqm-shortcode.php';
         require_once __DIR__ . '/class-wqm-storage.php';
+        require_once __DIR__ . '/class-wqb-hooks.php';
 
         new WQM_Shortcode();
         new WQM_Storage();
+        new WQM_Hooks();
     }
 }
