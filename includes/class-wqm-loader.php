d47729012e7842e8c3d805b3666f4c4973f6ca41
Adrian T. Saycon <adzbite@gmail.com>
2025-06-19 15:08:04 +0800

    Implement main loader class

---
+++ includes/class-wqm-loader.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Main loader for Bonza Quote plugin
+ */
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class WQM_Loader {
+    public function run() {
+
+    }
+}
--- wp-quote-management.php
+++ wp-quote-management.php
@@ -9,6 +9,15 @@ Author URI: https://adzbyte.com/adz
 Text Domain: wp-quote-management
 */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+if (!defined('ABSPATH')) {
+	exit;
 }
+
+require_once __DIR__ . '/includes/class-wqb-loader.php';
+
+function wqb_run_plugin()
+{
+	$loader = new WQB_Loader();
+	$loader->run();
+}
+add_action('plugins_loaded', 'wqb_run_plugin');
